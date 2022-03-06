90df7f0b981d01f80ad16ef01418cf6beb9ba026
Cerberus270 <geoavila718@gmail.com>
2022-03-06 10:44:46 -0600

    Test de modificacion

--- TareaS1CotizadorCripto_034519/Cotizador.aspx.cs
+++ TareaS1CotizadorCripto_034519/Cotizador.aspx.cs
@@ -26,18 +26,20 @@ namespace TareaS1CotizadorCripto_034519
                 string resultado = Consulta(cripto, moneda);
                 System.Diagnostics.Debug.WriteLine(resultado);
                 string price = Regex.Replace(resultado, @"[^\d,\.]", "");
-                //result.Text = "El precio es: " + cmbMoneda.SelectedItem.Value + "\t" + price;
+                // Se esta utilizando String Literal para facilitar la insercion de datos en el string
+                // pero a modo de prueba se deja, como se haria en la manera clasica
+                // result.Text = "El precio es: " + cmbMoneda.SelectedItem.Value + "\t" + price;
                 result.Text = $"El precio de {cmbCripto.SelectedItem.Value} es: {cmbMoneda.SelectedItem.Value} {price}";
             } else
             {
-                result.Text = "Debe seleccionar una divisa FIAT y una Cripto para recibir una cotizacion";
+                result.Text = "Seleccione una divisa FIAT y una Cripto para recibir una cotizacion";
             }
         }
 
         public string Consulta(string cripto, string moneda)
         {
             string uri = $"https://min-api.cryptocompare.com/data/price?fsym={ cripto }&tsyms={ moneda }";
-            //string uri = "https://min-api.cryptocompare.com/data/price?fsym="+cripto+"&tsyms="+moneda;
+            // string uri = "https://min-api.cryptocompare.com/data/price?fsym="+cripto+"&tsyms="+moneda;
             System.Diagnostics.Debug.WriteLine(uri);
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
             request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
